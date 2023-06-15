dbd89b988f6e45843daf1c7c115d325f5076607f
tommaso van der meer <tommasovdm@gmail.com>
2023-06-15 14:04:16 +0200

    Revert "Fixed problems with ROS2 Foxy"
    This reverts commit b5818fe03945566fceb58a1ff74b3e6bbe8b1e89.

--- launch/cafe_ros2.launch.py
+++ launch/cafe_ros2.launch.py
@@ -1,28 +1,117 @@
-from os import environ, path
+from os import environ
+from os import pathsep
+from scripts import GazeboRosPaths
 
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, DeclareLaunchArgument, ExecuteProcess, Shutdown
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution, LaunchConfiguration, PythonExpression, EnvironmentVariable
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-    pkg_dir = get_package_share_directory('gazebo_sfm_plugin')
-    my_gazebo_models = path.join(pkg_dir,'models')
-    print('my models:', my_gazebo_models)
+    my_gazebo_models = PathJoinSubstitution([
+        FindPackageShare('gazebo_sfm_plugin'),
+        'models',
+    ])
 
-    world_path = path.join(pkg_dir,'worlds','cafe3.world')
 
-    environ["GAZEBO_MODEL_PATH"] = my_gazebo_models
+    # src_gazebo_cmd = [
+    #     'source ',
+    #     '/usr/share/gazebo/setup.sh'
+    # ]
+    # ExecuteProcess(
+    #     cmd=src_gazebo_cmd,
+    #     #output='screen',
+    #     shell=True
+    # )
+    # print('gazebo_source:', src_gazebo_cmd)
 
-    gazebo = ExecuteProcess(
-            cmd=['gazebo', '--verbose', world_path, '-s', 'libgazebo_ros_init.so',
-            '-s', 'libgazebo_ros_factory.so'],
-            output='screen')
+    
+    model, plugin, media = GazeboRosPaths.get_paths()
+    #print('model:', model)
+
+    if 'GAZEBO_MODEL_PATH' in environ:
+        model += pathsep+environ['GAZEBO_MODEL_PATH']
+    if 'GAZEBO_PLUGIN_PATH' in environ:
+        plugin += pathsep+environ['GAZEBO_PLUGIN_PATH']
+    if 'GAZEBO_RESOURCE_PATH' in environ:
+        media += pathsep+environ['GAZEBO_RESOURCE_PATH']
+
+    env = {
+        'GAZEBO_MODEL_PATH': model,
+        'GAZEBO_PLUGIN_PATH': plugin,
+        'GAZEBO_RESOURCE_PATH': media
+    }
+    print('env:', env)
+
+    
+    world_path = PathJoinSubstitution([
+        FindPackageShare('gazebo_sfm_plugin'),
+        'worlds',
+        'cafe3.world'
+    ])
+
+    gzserver_cmd = [
+        'gzserver ',
+        #'-u ', #to start paused
+        # Pass through arguments to gzserver
+        LaunchConfiguration('world'), world_path, 
+        _boolean_command('verbose'), '',
+    ]
+
+    gzclient_cmd = [
+        'gzclient',
+        _boolean_command('verbose'), ' ',
+    ]
+
+    
+
 
     return LaunchDescription([
-        gazebo,
+
+        SetEnvironmentVariable(
+            name='GAZEBO_MODEL_PATH', 
+            value=[EnvironmentVariable('GAZEBO_MODEL_PATH'), my_gazebo_models]
+        ),
+        SetEnvironmentVariable(
+            name='GAZEBO_RESOURCE_PATH', 
+            value=[EnvironmentVariable('GAZEBO_RESOURCE_PATH'), my_gazebo_models]
+        ),
+        SetEnvironmentVariable(
+            name='GAZEBO_PLUGIN_PATH', 
+            value=[EnvironmentVariable('GAZEBO_PLUGIN_PATH'), plugin]
+        ),
+
+        DeclareLaunchArgument(
+            'world', default_value='',
+                     #'/home/kenny/ros2_ws/src/human_nav_gazebo_plugin/worlds/cafe2.world',
+            description='Specify world file name'
+        ),
+        DeclareLaunchArgument(
+            'verbose', default_value='true',
+            description='Set "true" to increase messages written to terminal.'
+        ),
+
+
+        ExecuteProcess(
+            cmd=gzserver_cmd,
+            output='screen',
+            #additional_env=env,
+            shell=True,
+            on_exit=Shutdown(),
+            #condition=IfCondition(LaunchConfiguration('server_required')),
+        ),
+
+        ExecuteProcess(
+            cmd=gzclient_cmd,
+            output='screen',
+            #additional_env=env,
+            shell=True,
+            on_exit=Shutdown(),
+            #condition=IfCondition(LaunchConfiguration('server_required')),
+        ),
     ])
 
 
